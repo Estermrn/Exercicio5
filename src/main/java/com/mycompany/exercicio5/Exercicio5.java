8305ff016d3d86771e9b09d7df177246da1f7e58
daros <daros@EsterMariani>
2025-04-08 18:31:59 -0300

    Inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio5/Exercicio5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio5;
+
+/**
+ *
+ * @author daros
+ */
+import javax.swing.JOptionPane;
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+       
+    int variavel = 20;
+    
+    JOptionPane.showMessageDialog(null,variavel);
+    }
+}
